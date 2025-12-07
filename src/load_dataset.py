fcd10e554b8e8bb85f3b7d1cdf9a0c440a89aeb1
devjwsong <devjwsong@gmail.com>
2025-12-06 21:36:14 -0800

    Change the dataset into UltraFeedback

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 import json
 import argparse
 import random
@@ -6,81 +6,88 @@ import os
 import math
 
 from tqdm import tqdm
-import kagglehub
+from langdetect import detect
+from datasets import load_dataset
 import numpy as np
 
-
-def _process_sample(sample: dict) -> dict:
-    new_sample = {
-        'name': sample['name'],
-        'summary': sample['short_description'],
-        'positive': sample['positive'],
-        'negative': sample['negative'],
-        'categories': sample['categories'],
-        'genres': sample['genres'],
-        'description': sample['detailed_description']
-    }
-    return new_sample
+DATASET_PATH = "openbmb/UltraFeedback"
+NUM_BINS = 10
 
 
-def _split_train_eval(pairs: List[Tuple[str, int]], train_ratio: float=0.8) -> Tuple[List, List]:
-    num_train_samples = int(len(pairs) * train_ratio)
-    return pairs[:num_train_samples], pairs[num_train_samples:]
+def _split_train_eval(samples: List[Dict], train_ratio: float=0.8) -> Tuple[List, List]:
+    num_train_samples = int(len(samples) * train_ratio)
+    return samples[:num_train_samples], samples[num_train_samples:]
 
 
 def main(args: argparse.Namespace):
-    # Download latest version
-    path = kagglehub.dataset_download("fronkongames/steam-games-dataset")
-    with open(path + "/games.json", 'r', encoding='utf-8') as f:
-        games = json.load(f)
-    print(f"Total number of samples: {len(games)}")
+    # Download from HuggingFace dataset repo.
+    ds = load_dataset(DATASET_PATH)
+    print(f"Total number of samples: {len(ds)}")
 
-    # Calculate the scores by positive - negative and store in bins.
-    game_ids = list(games.keys())
-    num_samples_per_bins = math.ceil(len(game_ids) / args.num_bins)
-    score_pairs = []
-    for game_id in tqdm(game_ids):
-        obj = games[game_id]
-        positive, negative = obj['positive'], obj['negative']
-        score_pairs.append((game_id, positive - negative))
-    score_pairs = sorted(score_pairs, key=lambda x:x[1])
-    bins = [[] for i in range(args.num_bins)]
-    cur_bin = 0
-    for game_id, score in score_pairs:
-        bins[cur_bin].append((game_id, score))
+    # Language filtering and separate by sources.
+    source2samples = {}
+    print(f"Leaving only English data...")
+    for obj in tqdm(ds['train']):
+        source = obj['source']
+        if source not in source2samples:
+            source2samples[source] = []
 
-        if len(bins[cur_bin]) == num_samples_per_bins:
-            cur_bin += 1
+        instruction = obj['instruction']
+        try:
+            if detect(instruction) == 'en':
+                completions = obj['completions']
+                for completion in completions:
+                    response = completion['response']
+                    if detect(response) == 'en':
+                        score = completion['overall_score']
+                        source2samples[source].append({
+                            'instruction': instruction,
+                            'response': response,
+                            'score': score
+                        })
 
-    print("[Score Distribution]")
-    for b, bin in enumerate(bins):
-        scores = [pair[1] for pair in bin]
-        print(f"Bin {b} =>")
-        print(f"Max: {np.max(scores)} || Min: {np.min(scores)} || Mean: {np.mean(scores)} || Median: {np.median(scores)}")
-        print()
+        except Exception as e:
+            print(e)
+    print(f"Numbers off samples after filtering + parsing:")
+    total_count = 0
+    for source, samples in source2samples.items():
+        print(f"{source}: {len(samples)}")
+        total_count += len(samples)
+    print(f"Total: {total_count}")
+    print()
 
-    # Random sampling & split.
+    # Random sampling per source and score.
     print("Random sampling & Splitting...")
     sft_train_set, sft_eval_set = [], []
     rm_train_set, rm_eval_set = [], []
-    ppo_train_set, ppo_eval_set = [], []
+    rlhf_train_set, rlhf_eval_set = [], []
     random.seed(args.seed)
-    for bin in bins:
-        random.shuffle(bin)
+    for source, samples in source2samples.items():
+        print(f"Processing source: {source}...")
+        bins = [[] for i in range(NUM_BINS)]
+        for sample in tqdm(samples):
+            score = sample['score']
+            bin_number = int(score)
+            bins[bin_number-1].append(sample)
 
-        # Split by stage.
-        num_sft_split, num_rm_split = int(len(bin) * args.sft_ratio), int(len(bin) * args.rm_ratio)
-        sft_split, rm_split, ppo_split = bin[:num_sft_split], bin[num_sft_split:num_sft_split+num_rm_split], bin[num_sft_split+num_rm_split:]
-        sft_train_split, sft_eval_split = _split_train_eval(sft_split, args.train_ratio)
-        rm_train_split, rm_eval_split = _split_train_eval(rm_split, args.train_ratio)
-        ppo_train_split, ppo_eval_split = _split_train_eval(ppo_split, args.train_ratio)
+        for b, bin in enumerate(bins):
+            print(f"Bin [{b+1}:{b+2}): {len(bin)}")
+            random.shuffle(bin)
 
-        sft_train_set += [_process_sample(games[game_id]) for game_id, _ in sft_train_split]
-        sft_eval_set += [_process_sample(games[game_id]) for game_id, _ in sft_eval_split]
-        rm_train_set += [_process_sample(games[game_id]) for game_id, _ in rm_train_split]
-        rm_eval_set += [_process_sample(games[game_id]) for game_id, _ in rm_eval_split]
-        ppo_train_set += [_process_sample(games[game_id]) for game_id, _ in ppo_train_split]
-        ppo_eval_set += [_process_sample(games[game_id]) for game_id, _ in ppo_eval_split]
+            # Split by stage.
+            num_sft_split, num_rm_split = int(len(bin) * args.sft_ratio), int(len(bin) * args.rm_ratio)
+            sft_split, rm_split, rlhf_split = bin[:num_sft_split], bin[num_sft_split:num_sft_split+num_rm_split], bin[num_sft_split+num_rm_split:]
+            sft_train_split, sft_eval_split = _split_train_eval(sft_split, args.train_ratio)
+            rm_train_split, rm_eval_split = _split_train_eval(rm_split, args.train_ratio)
+            rlhf_train_split, rlhf_eval_split = _split_train_eval(rlhf_split, args.train_ratio)
+
+            sft_train_set += sft_train_split
+            sft_eval_set += sft_eval_split
+            rm_train_set += rm_train_split
+            rm_eval_set += rm_eval_split
+            rlhf_train_set += rlhf_train_split
+            rlhf_eval_set += rlhf_eval_split
+        print()
 
     print(f"[Supervised Fine-Tuning]")
     print(f"Total: {len(sft_train_set) + len(sft_eval_set)}")
@@ -94,10 +101,10 @@ def main(args: argparse.Namespace):
     print(f"Eval: {len(rm_eval_set)}")
     print()
 
-    print(f"[RLHF: PPO Training]")
-    print(f"Total: {len(ppo_train_set) + len(ppo_eval_set)}")
-    print(f"Train: {len(ppo_train_set)}")
-    print(f"Eval: {len(ppo_eval_set)}")
+    print(f"[Reinforcement Learning from Human Feedback]")
+    print(f"Total: {len(rlhf_train_set) + len(rlhf_eval_set)}")
+    print(f"Train: {len(rlhf_train_set)}")
+    print(f"Eval: {len(rlhf_eval_set)}")
     print()
 
     # Save the data samples.
@@ -117,13 +124,13 @@ def main(args: argparse.Namespace):
     with open(f"{rm_data_dir}/eval_samples.json", 'w') as f:
         json.dump(rm_eval_set, f)
 
-    ppo_data_dir = args.data_dir + "/ppo"
-    if not os.path.isdir(ppo_data_dir):
-        os.makedirs(ppo_data_dir)
-    with open(f"{ppo_data_dir}/train_samples.json", 'w') as f:
-        json.dump(ppo_train_set, f)
-    with open(f"{ppo_data_dir}/eval_samples.json", 'w') as f:
-        json.dump(ppo_eval_set, f)
+    rlhf_data_dir = args.data_dir + "/rlhf"
+    if not os.path.isdir(rlhf_data_dir):
+        os.makedirs(rlhf_data_dir)
+    with open(f"{rlhf_data_dir}/train_samples.json", 'w') as f:
+        json.dump(rlhf_train_set, f)
+    with open(f"{rlhf_data_dir}/eval_samples.json", 'w') as f:
+        json.dump(rlhf_eval_set, f)
 
     print("Data loading done.")
 
@@ -135,7 +142,6 @@ if __name__=='__main__':
     parser.add_argument('--sft_ratio', type=float, default=0.4, help="The ratio of the data samples for supervised fine-tuning.")
     parser.add_argument('--rm_ratio', type=float, default=0.2, help="The ratio of the data samples for reward model training.")
     parser.add_argument('--train_ratio', type=float, default=0.8, help="The ratio of the data samples for train set.")
-    parser.add_argument('--num_bins', type=int, default=10, help="The number of bins to distribute the samples by scores.")
               
     args = parser.parse_args()
 
